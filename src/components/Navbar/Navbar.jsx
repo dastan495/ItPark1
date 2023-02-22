4ae3966da2436672bf9347cc160b35ce978872cb
unknown <timurshermamatov1@gmail.com>
2023-02-22 23:16:04 +0600

    .

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,11 +4,11 @@ import "../../adaptive_style.css";
 import logo from "../../images/logo.png";
 import { useNavigate } from "react-router-dom";
 const Navbar = () => {
-  const navigate = useNavigate();
-  return (
-    <div className="container">
-      {/* burger start  */}
-      {/* <div className="burger-menu">
+    const navigate = useNavigate();
+    return (
+        <div className="container">
+            {/* burger start  */}
+            {/* <div className="burger-menu">
         <div className="burger-menu__content">
           <img src={logo} className="burger-menu__btn_close" alt="" />
           <input type="text" className="burger-menu__input" />
@@ -28,47 +28,47 @@ const Navbar = () => {
           </ul>
         </div>
       </div> */}
-      {/* burger end  */}
-      {/* <!-- navbar start --> */}
-      <div className="navbar">
-        <img className="navbar__logo" src={logo} alt="navbar logo" />
-        <ul className="navbar__list">
-          <li className="list__item">
-            <a onClick={() => navigate("/")} href="#">
-              Главная
-            </a>
-          </li>
-          <li className="list__item">
-            <a onClick={() => navigate("/course")} href="">
-              курсы
-            </a>
-          </li>
-          <li className="list__item">
-            <a onClick={() => navigate("/metodic")} href="">
-              методика
-            </a>
-          </li>
-          <li className="list__item">
-            <a onClick={() => navigate("/price")} href="">
-              цена
-            </a>
-          </li>
-          <li className="list__item">
-            <a onClick={() => navigate("contacts")} href="">
-              контакты
-            </a>
-          </li>
-        </ul>
-        <input type="text" className="navbar__input" />
-        <img
-          src="./images/burger_btn.png"
-          alt=""
-          className="navbar__btn_burger"
-        />
-      </div>
-      {/* <!-- navbar end --> */}
-    </div>
-  );
+            {/* burger end  */}
+            {/* <!-- navbar start --> */}
+            <div className="navbar">
+                <img className="navbar__logo" src={logo} alt="navbar logo" />
+                <ul className="navbar__list">
+                    <li className="list__item">
+                        <a onClick={() => navigate("/")} href="#">
+                            Главная
+                        </a>
+                    </li>
+                    <li className="list__item">
+                        <a onClick={() => navigate("/course")} href="">
+                            курсы
+                        </a>
+                    </li>
+                    <li className="list__item">
+                        <a onClick={() => navigate("/metodic")} href="">
+                            методика
+                        </a>
+                    </li>
+                    <li className="list__item">
+                        <a onClick={() => navigate("/price")} href="">
+                            цена
+                        </a>
+                    </li>
+                    <li className="list__item">
+                        <a onClick={() => navigate("contacts")} href="">
+                            контакты
+                        </a>
+                    </li>
+                </ul>
+                <input type="text" className="navbar__input" />
+                <img
+                    src="./images/burger_btn.png"
+                    alt=""
+                    className="navbar__btn_burger"
+                />
+            </div>
+            {/* <!-- navbar end --> */}
+        </div>
+    );
 };
 
 export default Navbar;
